5a911a7bb982d827f0cbaa44debd15d9862f3fbf
nfskiy2 <strike2002.st@gmail.com>
2025-03-12 20:06:35 +0700

    блютуз модуль, весь код в мейн файле

--- main/main.c
+++ main/main.c
@@ -6,23 +6,129 @@
 EventGroupHandle_t event_group;
 
 
-// Главная функция
-void app_main(void) {
-    event_group = xEventGroupCreate(); // Создание группы событий
+// // Главная функция
+// void app_main(void) {
+//     event_group = xEventGroupCreate(); // Создание группы событий
     
-    motor_init();
-    motor_off();
+//     motor_init();
+//     motor_off();
     
-    status_led_init();
-    hall_sensor_init();
+//     status_led_init();
+//     hall_sensor_init();
 
-    ble_connect_vibro();
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    ble_disconnect_vibro();
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    notify_vibro();
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    notify_open_vibro();
+//     ble_connect_vibro();
+//     vTaskDelay(pdMS_TO_TICKS(2000));
+//     ble_disconnect_vibro();
+//     vTaskDelay(pdMS_TO_TICKS(2000));
+//     notify_vibro();
+//     vTaskDelay(pdMS_TO_TICKS(2000));
+//     notify_open_vibro();
 
-    xTaskCreate(event_task, "event_task", 2048, NULL, 10, NULL);
+//     xTaskCreate(event_task, "event_task", 2048, NULL, 10, NULL);
+// }
+
+
+
+char *TAG = "BLE-Server";
+uint8_t ble_addr_type;
+void ble_app_advertise(void);
+
+// Write data to ESP32 defined as server
+static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
+    return 0;
+}
+
+// Read data from ESP32 defined as server
+static int device_read(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    os_mbuf_append(ctxt->om, "Data from the server", strlen("Data from the server"));
+    return 0;
+}
+
+// Array of pointers to other service definitions
+// UUID - Universal Unique Identifier
+static const struct ble_gatt_svc_def gatt_svcs[] = {
+    {.type = BLE_GATT_SVC_TYPE_PRIMARY,
+     .uuid = BLE_UUID16_DECLARE(0x180),                 // Define UUID for device type
+     .characteristics = (struct ble_gatt_chr_def[]){
+         {.uuid = BLE_UUID16_DECLARE(0xFEF4),           // Define UUID for reading
+          .flags = BLE_GATT_CHR_F_READ,
+          .access_cb = device_read},
+         {.uuid = BLE_UUID16_DECLARE(0xDEAD),           // Define UUID for writing
+          .flags = BLE_GATT_CHR_F_WRITE,
+          .access_cb = device_write},
+         {0}}},
+    {0}};
+
+// BLE event handling
+static int ble_gap_event(struct ble_gap_event *event, void *arg)
+{
+    switch (event->type)
+    {
+    // Advertise if connected
+    case BLE_GAP_EVENT_CONNECT:
+        ESP_LOGI("GAP", "BLE GAP EVENT CONNECT %s", event->connect.status == 0 ? "OK!" : "FAILED!");
+        if (event->connect.status != 0)
+        {
+            ble_app_advertise();
+        }
+        break;
+    // Advertise again after completion of the event
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        ESP_LOGI("GAP", "BLE GAP EVENT");
+        ble_app_advertise();
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+// Define the BLE connection
+void ble_app_advertise(void)
+{
+    // GAP - device name definition
+    struct ble_hs_adv_fields fields;
+    const char *device_name;
+    memset(&fields, 0, sizeof(fields));
+    device_name = ble_svc_gap_device_name(); // Read the BLE device name
+    fields.name = (uint8_t *)device_name;
+    fields.name_len = strlen(device_name);
+    fields.name_is_complete = 1;
+    ble_gap_adv_set_fields(&fields);
+
+    // GAP - device connectivity definition
+    struct ble_gap_adv_params adv_params;
+    memset(&adv_params, 0, sizeof(adv_params));
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
+    ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
+}
+
+// The application
+void ble_app_on_sync(void)
+{
+    ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
+    ble_app_advertise();                     // Define the BLE connection
+}
+
+// The infinite task
+void host_task(void *param)
+{
+    nimble_port_run(); // This function will return only when nimble_port_stop() is executed
+}
+
+void app_main() {
+    nvs_flash_init();
+    esp_nimble_hci_init();  // Инициализация HCI (исправлено)
+    nimble_port_init();
+    ble_svc_gap_device_name_set("BLE-Server");
+    ble_svc_gap_init();
+    ble_gatts_count_cfg(gatt_svcs);
+    ble_gatts_add_svcs(gatt_svcs);
+    ble_svc_gatt_init();
+    ble_hs_cfg.sync_cb = ble_app_on_sync;
+    nimble_port_freertos_init(host_task);
 }
